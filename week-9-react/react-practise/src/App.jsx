f3cbc235369aa7782edc6a2125c12aa03bcd335f
viveksharma2105 <viveksharma02005@gmail.com>
2025-05-26 18:14:49 +0530

    StopWatch_React

---
+++ week-9-react/react-practise/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState,  useEffect } from 'react'
+import './App.css'
+
+function App() {
+  let [visible,setCount] =  useState(true);
+  
+  useEffect(function(){
+setInterval(()=>{
+  setCount(c => !c)
+},5000)
+  },[]);
+  
+
+ return <div>
+
+  {visible && <Counter></Counter>}
+
+ </div>
+
+}
+
+  function Counter() {
+  const [count, setCount]   = useState(0);
+
+//it is doingjuggling because  it is calling manytimes on each call
+//for this we use useEffect  to preventing re-rendering  and also it ensure the the function in 
+//useEffect is calls once 
+
+useEffect(function(){
+  setInterval(()=>{
+   setCount(count => count+1)
+   
+  },1000)
+ return function(){
+  clearInterval(clock)
+ }
+
+},[]);
+
+ 
+  
+  return<div >
+        <h1>{count}</h1>      
+      </div>
+   
+    
+  
+}
+
+
+export default App
