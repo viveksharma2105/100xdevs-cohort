495485fa6f2b874bcb1d52c41e9fb773c37f0c6b
viveksharma2105 <viveksharma02005@gmail.com>
2025-05-30 18:10:50 +0530

    theme changing button

--- week-9.3-react/react/src/App.jsx
+++ week-9.3-react/react/src/App.jsx
@@ -4,16 +4,39 @@ import './App.css'
 //--------FRAGMENTS OR EMPTY TAGS TO AVOID EXTRA DIV IN CASE OF INSERT MORE THEN ONE DIV------------------------
 
 
-function App(params) {
-  return <>
-<div>hi</div>
-<div>hi there</div>
-  </>
+// function App(params) {
+//   return <>
+// <div>hi</div>
+// <div>hi there</div>
+//   </>
   
-};
+// };
 
-export default App
+// export default App
 
+// ////--------------THEME CHANGING ----------------------
+// import { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//      const [darkMode, setDarkmode] = useState(false);
+//      function toggletheme() {
+//       setDarkmode(!darkMode)
+//      }
+
+//    return <div  style={{
+//       backgroundColor: darkMode ? 'black' : 'white',
+//       height: '100vh',
+//       textAlign: 'center',
+//       padding:50
+//     }}>
+//       <button onClick={toggletheme} style={{color: darkMode ? 'white' : 'black', backgroundColor: darkMode ? 'black':'white'}}>Change Theme</button>
+//    </div>
+
+   
+// }
+
+// export default App;
 
 
 
