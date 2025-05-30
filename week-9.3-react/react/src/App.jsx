95b70ca1705dadff78c822596e342cbd3b56db6b
viveksharma2105 <viveksharma02005@gmail.com>
2025-05-30 13:47:09 +0530

    children, errorboundry,fragments

---
+++ week-9.3-react/react/src/App.jsx
@@ -0,0 +1,70 @@
+
+
+import './App.css'
+//--------FRAGMENTS OR EMPTY TAGS TO AVOID EXTRA DIV IN CASE OF INSERT MORE THEN ONE DIV------------------------
+
+
+function App(params) {
+  return <>
+<div>hi</div>
+<div>hi there</div>
+  </>
+  
+};
+
+export default App
+
+
+
+
+////---------------ERRORBOUNDRY CLASS--------------------
+// class ErrorBoundry extends React.Component{
+//   constructor(props){
+//     super(props);
+//       this.state={hasError: false};
+    
+//   }
+//   static getDerivedStateFromError(error){
+//     return {hasError: true};
+//   }
+//   componentDidCatch(error, info){
+//     console.error("error caughed:", error, info);
+
+//   }
+//   render(){
+//     if(this.state.hasError){
+//       return <h1>Something  went wrong</h1>;
+//     }
+//     return this.props.children
+//   }
+  
+// }
+
+
+
+// //---------------CHILDREN-------------------------------
+// function App() {
+
+//  return <div  style={{padding:10, display:'flex',background:'grey'}}>
+//   <Card>
+//     Type your Post here
+//     <br /><br />
+//     <input type="text" />
+//   </Card>
+//    <Card>
+//     Hi there
+//   </Card>
+//  </div>
+
+// }
+
+// function Card({children}) {
+//   return <div style={{background:'white', borderRadius:10,padding:10,  margin:10}}>
+
+
+// {children}
+//   </div>
+  
+  
+// }
+// export default App
