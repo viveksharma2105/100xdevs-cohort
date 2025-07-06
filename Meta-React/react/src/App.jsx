4d07513baec8682fe715d2cd2cf827c88cc02414
viveksharma2105 <viveksharma02005@gmail.com>
2025-07-06 20:52:22 +0530

    Meta  React

---
+++ Meta-React/react/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+
+  return (
+    <>
+    <Example3/>
+    </>
+  )
+  
+}
+
+function Example3() {
+  const getRandomNum = () => Math.floor(Math.random() * 10) + 1;
+  const userInput = prompt('Hi, enter something:');
+
+  return (
+    <div className="heading">
+      <h1>
+        Here's a random number from 1 to 10: {getRandomNum()} — You entered: {userInput}
+      </h1>
+    </div>
+  );
+}
+
+export default App
