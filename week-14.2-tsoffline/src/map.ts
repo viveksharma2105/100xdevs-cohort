b4534e29cc365a397ee4bfa8e9a8469a1add9f4c
gauriiv <garimasharma4467@gmail.com>
2025-08-19 22:57:44 +0530

    map

--- week-14.2-tsoffline/src/map.ts
+++ week-14.2-tsoffline/src/map.ts
@@ -1,3 +1,20 @@
 const users = new Map()
 users.set("ram",{age:21,  name:"v", email:'hbivi@gamil'})
 users.set("tom",{age:22,  name:"t", email:'tom@gamil'})
+
+const  user = users.get('ram') ////to get
+users.delete('ram')
+
+
+// //// To specific the Map (keys and  values)
+// type  User= {
+//     name: string,
+//     age: number,
+//     email: string
+// }
+// const users = new Map<string, User>()
+// users.set("ram",{age:21,  name:"v", email:'hbivi@gamil'})
+// users.set("tom",{age:22,  name:"t", email:'tom@gamil'})
+
+// const  user = users.get('ram') ////to get
+// users.delete('ram')
