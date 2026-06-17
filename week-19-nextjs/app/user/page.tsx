9c6dc91faba6008dd3a1c51e8fc27e32d554a88d
Vivek Sharma <viveksharma02005@gmail.com>
2026-06-17 22:30:33 +0530

    chore: update dependencies for Next.js and Tailwind CSS
    - Bump Next.js version from 16.2.1 to 16.2.9
    - Add @tailwindcss/vite dependency
    - Update Tailwind CSS version from ^4 to ^4.3.1

--- week-19-nextjs/app/user/page.tsx
+++ week-19-nextjs/app/user/page.tsx
@@ -1,31 +1,66 @@
-"use  client"
-
 import axios from "axios";
-import { useEffect, useState } from "react";
 
-export default
-function User() {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState({});
-    
-    useEffect(()=>{
-        axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
-        .then(response =>{
-            setData(response.data);
-            setLoading(false);
-        })
-    },[])
+export default async function User() {
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
 
-    if(loading){
-        return <div>
-            loading...
+  const data = response.data;
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {data.name}</div>
+          <div>Email: {data.email}</div>
         </div>
-    }
-
-    return <div>
-        User Page
-        {data.name}
-        {data.email}
+      </div>
     </div>
-
+  );
 }
+
+////not ssr because google see loading first also its clientside due to  useeffect
+// "use client";
+
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+
+// export default function User() {
+//   const [loading, setLoading] = useState(true);
+//   const [data, setData] = useState({});
+
+//   useEffect(() => {
+//     axios
+//       .get(
+//         "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+//       )
+//       .then((response) => {
+//         setData(response.data);
+//         setLoading(false);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="flex flex-col justify-center h-screen">
+//       <div className="flex justify-center">
+//         <div className="border p-8 rounded">
+//           <div>
+//             Name: {data.name}
+//           </div>
+
+//           <div>
+//             Email: {data.email}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
